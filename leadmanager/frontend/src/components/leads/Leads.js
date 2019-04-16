2fa08fd6466dc71d1cd7cfbacfc0a7bfeb02e1f4
TecateWine <tecatewine@protonmail.com>
2019-04-15 18:10:50 -0700

    Webpack and frontend nav

---
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class Leads extends Component {
+  render() {
+    return (
+    <div>
+        <h1>Leads List</h1>
+    </div>
+    )
+  }
+}
+
+
+
+
+export default Leads
